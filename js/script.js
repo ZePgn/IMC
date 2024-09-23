0b508f65659af01843b37ea72576f16f55999cfd
Felipe <zepagono@gmail.com>
2024-09-22 23:32:05 -0300

    versao 2
    arrumado a calculadora

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-function calcularIMC() {
-    var altura = document.getElementById('altura').value / 100; // convertendo cm para metros
-    var peso = document.getElementById('peso').value;
+function calcularIMC(peso, altura) {
     var imc = peso / (altura * altura);
-    alert("Seu IMC é: " + imc.toFixed(2));
+
+    console.log("Seu IMC é: " + imc.toFixed(2));
+
+    if (imc < 18.5) {
+        console.log("Você está abaixo do peso.");
+    } else if (imc >= 18.5 && imc < 24.9) {
+        console.log("Você está com peso normal.");
+    } else if (imc >= 25 && imc < 29.9) {
+        console.log("Você está com sobrepeso.");
+    } else if (imc >= 30) {
+        console.log("Você está com obesidade.");
+    }
 }
+
